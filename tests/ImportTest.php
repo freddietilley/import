c8846bf3df9c046d75defedea360f0851ec9f182
Bob Olde Hampsink <b.oldehampsink@itmundi.nl>
2014-09-03 11:17:25 +0200

    Special unit tests for AirBP

--- tests/ImportTest.php
+++ tests/ImportTest.php
@@ -1,76 +1,36 @@
 <?php
 namespace Craft;
 
-class ImportTest extends BaseTest 
+class ImportTest extends \UnitTestCase 
 {
     
-    protected $importHistoryService;
-    protected $importService;
-    
-    public function setUp()
+    function testHistoryShowLog() 
     {
-    
-        // Get dependencies
-        $dir = __DIR__;
-        $map = array(
-            '\\Craft\\ImportModel'           => '/../models/ImportModel.php',
-            '\\Craft\\Import_EntriesRecord'  => '/../records/Import_EntriesRecord.php',
-            '\\Craft\\Import_HistoryRecord'  => '/../records/Import_HistoryRecord.php',
-            '\\Craft\\Import_LogRecord'      => '/../records/Import_LogRecord.php',
-            '\\Craft\\Import_HistoryService' => '/../services/Import_HistoryService.php',
-            '\\Craft\\ImportService'         => '/../services/ImportService.php'
-        );
+        $log = craft()->import_history->showLog(1);
 
-        // Inject them
-        foreach($map as $classPath => $filePath) {
-            if(!class_exists($classPath, false)) {
-                require_once($dir . $filePath);
-            }
-        }
-    
-        // Construct them
-        $this->importHistoryService = new Import_HistoryService;
-        $this->importService        = new ImportService;
-    
-    } 
-    
-    public function testHistoryShowLog() 
-    {
-    
-        $log = $this->importHistoryService->showLog(1);
-        
         $this->assertTrue(is_array($log));
         $this->assertTrue($log > 0);
-        
     }
     
-    public function testHistoryLog() 
+    function testHistoryLog() 
     {
-    
-        $log = $this->importHistoryService->log($historyId = 1, $line = 0, $errors = array());
-        
+        $log = craft()->import_history->log($historyId = 1, $line = 0, $errors = array());
         $this->assertSame($log, $errors);
-        
     }
     
-    public function testPrepForElementModel()
+    function testPrepForEntryModel()
     {
-    
         $fields = array('title' => 'test');
-        $entry = $this->importService->prepForElementModel($fields, new EntryModel());
+        $entry = craft()->import_entry->prepForElementModel($fields, new EntryModel());
         
         $this->assertTrue($entry instanceOf EntryModel);
-        
     }
     
-    public function testPrepForFieldType()
+    function testPrepForFieldType()
     {
-    
         $data = ' u0';
-        $this->importService->prepForFieldType($data, 'price');
-        
+        craft()->import->prepForFieldType($data, 'price');
         $this->assertTrue(is_numeric($data));
-        
     }
     
 }
